2ce949a5f13662de97a48ef46b02a969b04604e7
shrabasti <shrabastichakraborty16@gmail.com>
2026-03-06 16:34:21 +0530

    docs: Add documentation for MinervaException class

--- src/main/java/minerva/data/exception/MinervaException.java
+++ src/main/java/minerva/data/exception/MinervaException.java
@@ -1,6 +1,18 @@
 package minerva.data.exception;
 
-public class MinervaException extends Exception{
+/**
+ * Represents an exception specific to the Minerva application.
+ *
+ * <p>This exception is thrown when user input is invalid or when an
+ * operation cannot be completed due to incorrect command formatting.</p>
+ */
+public class MinervaException extends Exception {
+
+    /**
+     * Constructs a {@code MinervaException} with the specified error message.
+     *
+     * @param errorMessage the message describing the cause of the exception
+     */
     public MinervaException(String errorMessage) {
         super(errorMessage);
     }
